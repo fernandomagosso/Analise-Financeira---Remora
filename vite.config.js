2f4f7b819143b791f58337f6a11844ba3cfd0b4b
fernandomagosso <fernandomagosso@gmail.com>
2025-12-14 09:12:45 -0300

    feat: Update Vite config and entry point
    Switches the entry point to `index.tsx` and sets the `base` configuration to `./` in `vite.config.js`. This change resolves issues with asset loading when deploying the application with relative paths.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Corrige problemas de carregamento de assets em caminhos relativos
   server: {
     host: true,
     hmr: {
